213684d6ed0bebcd02cdcc8462669091d60dfb05
Abigor24 <Abigor24@users.noreply.github.com>
2021-04-27 12:14:09 +0600

    create banner informs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 let menu = document.querySelector(".header__menu");
+let dropMenu = document.querySelector(".header__drom-menu");
 menu.addEventListener("click", function () {
     menu.classList.toggle("active");
+    dropMenu.classList.toggle("active");
 })
