5abb064ee8a560d3b0a027903dfe29f3410192dd
Abigot24 <jefr.p@yandex.ru>
2022-04-15 09:09:55 +0600

    create skill box

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,22 @@ let dropMenu = document.querySelector(".header__drom-menu");
 menu.addEventListener("click", function () {
     menu.classList.toggle("active");
     dropMenu.classList.toggle("active");
-})
+});
+
+let  numberPercent = document.querySelectorAll('.countbar')
+let getPercent = Array.from(numberPercent)
+
+getPercent.map((items) => {
+    let startCount = 0
+    let progressBar = () => {
+        startCount ++
+        items.innerHTML = `<h3>${startCount}%</h3>`
+        items.style.width = `${startCount}%`
+        if(startCount == items.dataset.percentnumber) {
+            clearInterval(stop)
+        }
+    }
+    let stop = setInterval(() => {
+        progressBar()
+    },25)
+});
